ae032279ab066a28d5f72e7e6c86e10861af6c47
SirFuzzyWalls <alexzandermcgraw@gmail.com>
2021-02-02 07:06:33 -0800

    finished basic input validation, you can no longer enter 8008 as a binary number

--- binary2decimal.cpp
+++ binary2decimal.cpp
@@ -20,18 +20,39 @@ int main()
         std::cout << "Please enter up to 8 binary digits now: ";
         std::cin >> inBinary;
 
+       
         //very basic input validation
-        std::cin.ignore(200, '\n'); 
+        bool invalidEntry = 0;
+        do
+        {
+            for (int i = 0; i < inBinary.length(); i++) {
+                if (inBinary[i] != '1' && inBinary[i] != '0') {
+                    invalidEntry = 1;
+                }
+            }
+            if (invalidEntry == 1) {
+                std::cout << "Error: Invalid digit found in string of binary digits, \nplease re-enter up to 8 binary digits: ";
+                std::cin.clear();
+                std::cin.ignore(200, '\n');
+                std::cin >> inBinary;
+            }
+            
+        } while (invalidEntry != 0);
+
         decimal = convertToDecimal(inBinary); 
+
         std::cout << "\nYou entered: " << inBinary << std::endl;
         std::cout << "In decimal form that is: " << decimal << std::endl;
         std::cout << "Would you like to enter another binary number? (y for yes, n for no): ";
         std::cin >> choice;
+        
     }
 
 }
 /*
 Takes the incomming string and converts it to a decimal value, stored as an int, then returns said value.
+Pre: Incomming string has already been validated and does not confirm any digits other than a 1 or a 0, IE 
+     it's actually a binary number. 
 */
 int convertToDecimal(const std::string inString)
 {
